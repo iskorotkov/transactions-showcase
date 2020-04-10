ace18a18aeeaa6c6099b7dc38362cbbd97245263
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-04-10 10:32:27 +0500

    Add option to select isolation level

--- TransactionsShowcase/Controls/TransactionControl.xaml.cs
+++ TransactionsShowcase/Controls/TransactionControl.xaml.cs
@@ -11,13 +11,29 @@ namespace TransactionsShowcase.Controls
     /// </summary>
     public partial class TransactionControl : UserControl
     {
+        private IsolationLevel _isolationLevel = IsolationLevel.Unspecified;
+
         public TransactionControl()
         {
             InitializeComponent();
+
+            IsolationLevelBox.Items.Add(IsolationLevel.ReadUncommitted);
+            IsolationLevelBox.Items.Add(IsolationLevel.ReadCommitted);
+            IsolationLevelBox.Items.Add(IsolationLevel.RepeatableRead);
+            IsolationLevelBox.Items.Add(IsolationLevel.Serializable);
         }
 
         public EmpiresManager EmpiresManager { get; set; }
-        public IsolationLevel IsolationLevel { get; set; } = IsolationLevel.Unspecified;
+
+        public IsolationLevel IsolationLevel
+        {
+            get => _isolationLevel;
+            set
+            {
+                _isolationLevel = value;
+                IsolationLevelBox.SelectedItem = value;
+            }
+        }
 
         private void BeginTransaction(object sender, RoutedEventArgs e)
         {
@@ -37,6 +53,11 @@ namespace TransactionsShowcase.Controls
         private void Commit(object sender, RoutedEventArgs e)
         {
             MessageOnException.Execute(() => EmpiresManager.CommitTransaction());
+        }
+
+        private void OnIsolationLevelSelected(object sender, SelectionChangedEventArgs e)
+        {
+            _isolationLevel = (IsolationLevel) IsolationLevelBox.SelectedItem;
         }
     }
 }
